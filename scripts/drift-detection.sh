2214b8bcb4f844dcb67aab966957871c5525a4fe
Rey <andrej.kolic@gmail.com>
2025-07-11 16:15:53 +0200

    Log input parameters; add drift detection script; update drift detetion condition

---
+++ scripts/drift-detection.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+
+# Default values
+CONFIG_FILE="deploy-config.json"
+ENVIRONMENT=${1:-dev}
+REGION="us-east-1"              # <-- Change this to your region
+STACK_NAME="static-website-dev" # <-- Change this to your stack name
+
+
+# Get configuration for environment
+env=$1
+if ! jq -e ".${env}" "$CONFIG_FILE" > /dev/null 2>&1; then
+    echo "Configuration for environment '${env}' not found in ${CONFIG_FILE}"
+    exit 1
+fi
+
+STACK_NAME=$(jq -r ".${env}.stackName" "$CONFIG_FILE")
+REGION=$(jq -r ".${env}.region" "$CONFIG_FILE")
+
+
+# drift detection
+echo "Initiating drift detection for stack: $STACK_NAME in region: $REGION"
+DRIFT_ID=$(aws cloudformation detect-stack-drift --stack-name "$STACK_NAME" --region "$REGION" --query "StackDriftDetectionId" --output text)
+
+if [ -z "$DRIFT_ID" ]; then
+  echo "Error: Could not initiate drift detection."
+  exit 1
+fi
+
+echo "Drift detection initiated. ID: $DRIFT_ID"
+echo "Waiting for drift detection to complete..."
+
+STATUS="DETECTION_IN_PROGRESS"
+while [ "$STATUS" == "DETECTION_IN_PROGRESS" ]; do
+  sleep 10 # Wait for 10 seconds before checking again
+  RESPONSE=$(aws cloudformation describe-stack-drift-detection-status \
+    --stack-drift-detection-id "$DRIFT_ID" \
+    --region "$REGION" \
+    --query "[DetectionStatus, StackDriftStatus]" --output text)
+
+  STATUS=$(echo "$RESPONSE" | awk '{print $1}')
+  STACK_DRIFT_STATUS=$(echo "$RESPONSE" | awk '{print $2}')
+
+  echo "Current status: $STATUS, Stack Drift Status: ${STACK_DRIFT_STATUS:-N/A}" # N/A for when still IN_PROGRESS
+
+  if [ "$STATUS" == "DETECTION_FAILED" ]; then
+    echo "Drift detection failed!"
+    exit 1
+  fi
+done
+
+echo "Drift detection complete. Final Stack Drift Status: $STACK_DRIFT_STATUS"
+
+if [ "$STACK_DRIFT_STATUS" == "DRIFTED" ]; then
+  echo "Stack has drifted! Listing drifted resources:"
+  aws cloudformation describe-stack-resource-drifts --stack-name "$STACK_NAME" --region "$REGION" | jq
+else
+  echo "Stack is IN_SYNC (no drift detected)."
+fi
